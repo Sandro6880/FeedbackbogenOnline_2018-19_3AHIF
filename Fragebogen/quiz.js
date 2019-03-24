f5677c54cecf03fb2034ad330175cbabb95ca6c4
RaciDionis <dionis_4@hotmail.com>
2019-03-24 16:24:11 +0100

    deleted unnecessary lines of code which were only test purposes

--- Fragebogen/quiz.js
+++ Fragebogen/quiz.js
@@ -1,14 +1,10 @@
 var answers = "";
-// function to calculate the result of the survey
+
 function tabulateAnswers() {
   var choices = document.getElementsByTagName('question');
   var answerOfUser = document.getElementsByTagName('input');
-  var answerbox = document.getElementById('answer');
-  answerbox.innerHTML = choices.length;
   var offset = 0;
   for (i=0; i<answerOfUser.length; i++) {
-     // if the radio is checked..
-       // add 1 to that choice's score
     if (answerOfUser[i].checked) {
       var done = false
       let x = 0;
@@ -27,7 +23,7 @@ function tabulateAnswers() {
    var saved = false;
    while (!saved) {
      if (localStorage.getItem(i) === null) {
-       window.localStorage.setItem(i, answers);
+       localStorage.setItem(i, answers);
        saved = true;
      }
      i++;
