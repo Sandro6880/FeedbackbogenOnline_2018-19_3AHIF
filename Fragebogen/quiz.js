c11d13e0527edd7d26ee0a44d340d813d21ef7f6
RaciDionis <dionis_4@hotmail.com>
2019-03-24 14:24:34 +0100

    Just a test

---
+++ Fragebogen/quiz.js
@@ -0,0 +1,13 @@
+let i = 0;
+var saved = false;
+while (!saved) {
+  if (localSorage.getElementById(i) === NULL) {
+    localStorage.setItem(person, 0);
+    saved = true;
+  }
+  i++;
+}
+
+// function to calculate the result of the survey
+function tabulateAnswers() {
+}
