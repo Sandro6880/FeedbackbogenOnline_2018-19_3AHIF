1e40de7bd8abbf38b34ec14c2ca00dcf9da6d5b3
RaciDionis <dionis_4@hotmail.com>
2019-03-24 15:14:52 +0100

    made a test using localstoragen Need to fix: make fields required.

--- Fragebogen/quiz.js
+++ Fragebogen/quiz.js
@@ -1,13 +1,35 @@
-let i = 0;
-var saved = false;
-while (!saved) {
-  if (localSorage.getElementById(i) === NULL) {
-    localStorage.setItem(person, 0);
-    saved = true;
-  }
-  i++;
-}
-
+var answers = "";
 // function to calculate the result of the survey
 function tabulateAnswers() {
+  var choices = document.getElementsByTagName('question');
+  var answerOfUser = document.getElementsByTagName('input');
+  var answerbox = document.getElementById('answer');
+  answerbox.innerHTML = choices.length;
+  var offset = 0;
+  for (i=0; i<answerOfUser.length; i++) {
+     // if the radio is checked..
+       // add 1 to that choice's score
+    if (answerOfUser[i].checked) {
+      var done = false
+      let x = 0;
+      while (!done) {
+        x++;
+        var c = "c" + x;
+        if (answerOfUser[i].value == c) {
+          answers += x + ",";
+          done = true;
+        }
+      }
+     }
+   }
+   answers += "0";
+   var i = 0;
+   var saved = false;
+   while (!saved) {
+     if (localStorage.getItem(i) === null) {
+       window.localStorage.setItem(i, answers);
+       saved = true;
+     }
+     i++;
+   }
 }
