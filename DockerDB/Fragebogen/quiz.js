f0b401c8c1b655c1a402365a1058e3d453e05947
sandro6880 <sandro6880@gmail.com>
2019-06-20 16:47:39 +0200

    asd

--- DockerDB/Fragebogen/quiz.js
+++ DockerDB/Fragebogen/quiz.js
@@ -1,19 +1,22 @@
-function myFunction() {
-  $("#sub").click(function() {
-   $.post( $("#quiz").attr("action"),
-           $("#quiz :input").serializeArray(),
-           function(info){ $("#result").html(info);
-     });
-   clearInput();
-  });
+var answers = "";
+// function to calculate the result of the survey
+function tabulateAnswers() {
+  var choices = document.getElementsByTagName('question');
+  var answerOfUser = document.getElementsByTagName('input');
 
-  $("#quiz").submit( function() {
-    return false;
-  });
-
-  function clearInput() {
-  	$("#quiz :input").each( function() {
-  	   $(this).val('');
-  	});
+  var offset = 0;
+  for (var i=0; i<choices.length; i++) {
+    var done = false;
+    for (var x = 0+offset; x < 4+offset; x++) {
+      if (answerOfUser[x].checked && !done) {
+        answers+=answerOfUser[x].value[1];
+        done = true;
+      }
+    }
+    offset+=4;
+    if (!done) {
+      answers+="0";
+    }
   }
-}
+   window.location.href = "questioninfo.php?answers=" + answers;
+   }
