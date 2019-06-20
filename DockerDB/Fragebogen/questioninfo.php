be835fd2944658b4873a94d843504d93c51f9747
sandro6880 <sandro6880@gmail.com>
2019-06-20 16:47:39 +0200

    asd

--- DockerDB/Fragebogen/questioninfo.php
+++ DockerDB/Fragebogen/questioninfo.php
@@ -1,54 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Php</title>
+  </head>
+  <body>
 <?php
-echo phpinfo();
-/* PDO user API gives no access to connection thread id */
-/*$mysql_connection = new PDO("mysql:host=localhost;dbname=mydb", "user", "passme");
+    $conn = mysqli_connect('mariadb:3306', 'root', 'passme', 'mydb');
+		if (!$conn) {
+    	echo('Verbindung schlug fehl: ');
+		}
+    $variable = $_GET["answers"];
+    if (isset($_GET["test"])) {
+      echo "E R R O R";
+    }
+    for ($i=1; $i < strlen($variable)+1; $i++) {
+      $result = mysqli_query($conn,"SELECT * FROM answer WHERE question LIKE 'q$i'");
+      if ($result->num_rows < 1) {
+        mysqli_query($conn, "INSERT INTO answer (question) VALUES ('q$i')");
+      }
+      echo "</br>";
+      $z = $i-1;
+      mysqli_query($conn,"UPDATE answer SET c$variable[$z] = c$variable[$z]+1 WHERE question = 'q$i'");
+    }
 
-/* Convert PDO MySQL handle to mysqlnd handle
-$mysqlnd = mysqlnd_uh_convert_to_mysqlnd($mysql_connection);
-
-/* Create Proxy to call mysqlnd connection class methods
-$obj = new MySQLndUHConnection();
-/* Call mysqlnd_conn::get_thread_id
-var_dump($obj->getThreadId($mysqlnd));
-
-/* Use SQL to fetch connection thread id
-var_dump($mysql_connection->query("SELECT CONNECTION_ID()")->fetchAll());
-/*$link = mysqli_connect("localhost:8000", "user", "passme", "mydb");
-
-if (!$link) {
-    echo "Fehler: konnte nicht mit MySQL verbinden." . PHP_EOL;
-    echo "Debug-Fehlernummer: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debug-Fehlermeldung: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Erfolg: es wurde ordnungsgemäß mit MySQL verbunden! Die Datenbank \"datenbank\" ist toll." . PHP_EOL;
-echo "Host-Informationen: " . mysqli_get_host_info($link) . PHP_EOL;
-
-mysqli_close($link);
-*/
-/*   include_once('db.php');
-
-
-    if(mysql_query("INSERT INTO answer VALUES('$class')"))
-      echo "Successfully inserted";
-    else
-      echo "Insertion failed";
-
-    mysql_close($conn);
-    <?php
-      $servername = "mariadb:3306";
-      $username = "user";
-      $password = "passme";
-
-    // Create connection
-      $conn = new mysql($servername, $username, $password);
-
-      // Check connection
-      if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-        }
-      echo "Connected successfully";
-    ?>
-*/
+    mysqli_close($conn);
  ?>
+</body>
+</html>
